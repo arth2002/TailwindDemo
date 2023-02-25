c94213cad11c182c18e149ba6ab0c570088ee1cc
arth2002 <arthprajapati6602@gmail.com>
2023-02-25 19:02:29 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'playfair':'Playfair Display',
+      },
+      colors:{
+        'nav':'#FFFFFF',
+        'navline':'#D0D0A7',
+        'home':'#E7F2ED',
+        'color2':'rgba(242, 242, 230, 1)',
+        'bcolor':'#0B8497',
+        'arrow':'#0B8497',
+      }
+    },
+  },
+  plugins: [],
+}
